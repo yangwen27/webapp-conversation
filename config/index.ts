79abb30fde210f947e904fc14d24cd92b2b5fe57
wooyang <myfox_yang@foxmail.com>
2025-03-04 20:11:24 +0800

    Update index.ts
    333

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
+  title: '讯驰-霍兰德测试',
   description: '',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en',
+  default_language: 'zh-cn',
 }
 
 export const isShowPrompt = false
